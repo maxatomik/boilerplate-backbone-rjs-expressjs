e366b9454cfd50e67bad19a4eed08e90c6270518
Maximilien Borne <maximilien.borne@gmail.com>
2016-06-06 15:37:43 +0200

    fix

--- src/NodeJS/controllers/page.js
+++ src/NodeJS/controllers/page.js
@@ -10,8 +10,8 @@ var express = require('express'),
 module.exports = function(data) {
  	router.get('/:page', function(req, res) {
       content = JSON.parse(fs.readFileSync(path.join(__dirname, "/../db/collection.json"), 'utf8'));
-      content['/page'+req.path].domainUrl = req.get('host');
       if(content['/page'+req.path] !== undefined) {
+         content['/page'+req.path].domainUrl = req.get('host');
 	       res.render('pages/page-layout-1.jade', content['/page'+req.path]); 
       } else {
         res.send('json not found, add /page'+req.path+' node into your collection.json');
