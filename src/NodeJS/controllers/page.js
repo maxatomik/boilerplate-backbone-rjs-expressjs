660b16f81ac648ee076c8c2f34ba424bd8a68234
Maximilien Borne <maximilien.borne@gmail.com>
2016-06-07 10:22:21 +0200

    fix

--- src/NodeJS/controllers/page.js
+++ src/NodeJS/controllers/page.js
@@ -8,31 +8,27 @@ var express = require('express'),
     availableTpl = [],
     tpl = "page-layout-1.jade";
 
-    fs.readdir("./web/www/templates/pages", function (err, files) { 
-      if (!err) {
-          for(var i = 0; i < files.length; i++ ){
-            availableTpl[files[i]] = 'ok';
-            console.log(availableTpl);
-          }
-      } else {
-          throw err; 
-        }
-    });
+fs.readdir("./web/www/templates/pages", function (err, files) { 
+  if (!err) {
+      for(var i = 0; i < files.length; i++ ){
+        availableTpl[files[i]] = 'ok';
+      }
+  } else {
+      throw err; 
+  }
+});
     
 module.exports = function(data) {
  	router.get('/:page', function(req, res) {
-
-  console.log(availableTpl['page-layout-1.jade'])
       content = JSON.parse(fs.readFileSync(path.join(__dirname, "/../db/collection.json"), 'utf8'));
       if(content['/page'+req.path] !== undefined) {
          if(content['/page'+req.path].tpl !== undefined ) tpl = content['/page'+req.path].tpl;
          content['/page'+req.path].domainUrl = req.get('host');
          if(availableTpl[tpl] !== undefined) {
-             res.render('pages/'+tpl, content['/page'+req.path]);
+            res.render('pages/'+tpl, content['/page'+req.path]);
          } else {
-           res.send('ok');
+            res.render('pages/page-layout-1.jade');
          }
-	      
       } else {
         res.send('json not found, add /page'+req.path+' node into your collection.json');
       }
