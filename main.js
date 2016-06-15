d22839759578a3ea687271d785470a9450226e0a
Maximilien Borne <mborne@frparpww1196.global.publicisgroupe.net>
2016-06-15 16:22:23 +0200

    fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var pm2 = require('pm2');
 var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
 var maxMemory = process.env.WEB_MEMORY || 512;    // " " "
 pm2.connect(function() {
-  pm2.start(__dirname + '/ecosystem.json', { "env" : process.env.NODE_ENV }, function(err) {
+  pm2.start(__dirname + '/ecosystem.json', function(err) {
     if (err) return console.error('Error while launching applications', err.stack || err);
     console.log('PM2 and application has been succesfully started');
     
